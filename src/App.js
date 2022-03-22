f6e6f522ada80a00e07904943fba5b87705a7c12
SnuggleTrouble <stabjo@outlook.com>
2022-03-22 13:03:23 +0100

    iteration 1 & 2 complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 // src/App.js
 import './App.css';
 import foods from './foods.json';
+import { FoodBox } from './components/FoodBox/FoodBox';
 
 function App() {
   return <div className="App">
     {foods.map((food) => {
       return (
         <div>
-          <h3>{food.name}</h3>
-          <img src={food.image} alt="food display" width={100}/>
+          <p>{food.name}</p>
+          <img src={food.image} alt="food" width={0}/>
         </div>
       )
-      
     })}
+    <FoodBox food={ {
+  name: "Orange",
+  calories: 85,
+  image: "https://i.imgur.com/abKGOcv.jpg",
+  servings: 1
+}} />
   </div>;
 }
 export default App;
