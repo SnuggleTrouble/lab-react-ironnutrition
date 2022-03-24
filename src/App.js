aafef23fdea7e78a78c49dc640db178978f3d357
SnuggleTrouble <stabjo@outlook.com>
2022-03-24 13:22:01 +0100

    iteration 4 complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 // src/App.js
 import './App.css';
-import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import React from 'react';
+import { useState } from 'react';
+import { Row, Divider, Button } from 'antd';
 import foods from './foods.json';
 import { FoodBox } from './components/FoodBox/FoodBox';
+import { AddFoodForm } from './components/AddFoodForm/AddFoodForm';
 
 function App() {
+  const [foodList, setFoodList] = useState(foods);
+  const addNewFood = (newFoodItem) => {
+    const amendedFoodList = [...foodList, newFoodItem];
+    setFoodList(amendedFoodList);
+  };
   return (
     <div className="App">
-    <Divider> Food List </Divider>
-    <Row style={{justifyContent: "center", textAlign: "center", width: "100vw"}}>
-      {foods.map((food) => {
-        return (
-          <FoodBox
-            food={{
-              name: food.name,
-              calories: food.calories,
-              image: food.image,
-              servings: food.servings,
-            }}
-          />
-        );
-      })}
+      {/* The Add Food Component */}
+      <AddFoodForm addFood={addNewFood} />
+
+      <Button> Hide Form / Add New Food </Button>
+
+      {/* The Search Component */}
+
+      <Divider> Food List </Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {/* List of Food Box components */}
+        {foods.map((food) => {
+          return (
+            <FoodBox
+              food={{
+                name: food.name,
+                calories: food.calories,
+                image: food.image,
+                servings: food.servings,
+              }}
+            />
+          );
+        })}
       </Row>
     </div>
   );
---
+++ src/components/AddFoodForm/AddFoodForm.js
@@ -0,0 +1,67 @@
+import { Divider, Input } from 'antd';
+import React from 'react';
+import { useState } from 'react';
+
+export function AddFoodForm(props) {
+  const [name, setName] = useState('');
+  const [image, setimage] = useState('');
+  const [calories, setCalories] = useState(0);
+  const [servings, setServings] = useState(0);
+
+  const handleNameInput = (e) => setName(e.target.value);
+  const handleImageInput = (e) => setimage(e.target.value);
+  const handleCaloriesInput = (e) => setCalories(e.target.value);
+  const handleServingsInput = (e) => setServings(e.target.value);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const newFoodItem = { name, image, calories, servings };
+    props.addFood(newFoodItem);
+    setName('');
+    setimage('');
+    setCalories(0);
+    setServings(0);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <Divider>Add Food Entry</Divider>
+
+        <label>Name</label>
+        <Input
+          value={name}
+          type="text"
+          name="name"
+          onChange={handleNameInput}
+        ></Input>
+
+        <label>Image</label>
+        <Input
+          value={image}
+          type="text"
+          name="image"
+          onChange={handleImageInput}
+        ></Input>
+
+        <label>Calories</label>
+        <Input
+          value={calories}
+          type="number"
+          name="calories"
+          onChange={handleCaloriesInput}
+        ></Input>
+
+        <label>Servings</label>
+        <Input
+          value={servings}
+          type="number"
+          name="servings"
+          onChange={handleServingsInput}
+        ></Input>
+
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
+}
